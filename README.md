4201bae309e6db8ccf86477f9de98794206ed868
stf_34 <shao_34@163.com>
2020-10-27 11:22:08 +0800

    测试

--- README.md
+++ README.md
@@ -22,3 +22,5 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 创建步骤和内容
--- src/components/hSteps/index.vue
+++ src/components/hSteps/index.vue
@@ -1,48 +1,112 @@
 <template>
-  <div class="steps" >
+  <div class="steps">
     <div
-      v-bind:class="[stepArray.length==2 ? 'width-2' : stepArray.length==3?'width-3':'width-4', '']"
-      v-for="(item,index) in stepArray"
+      v-bind:class="[
+        stepArray.length == 2
+          ? 'width-2'
+          : stepArray.length == 3
+          ? 'width-3'
+          : 'width-4',
+        '',
+      ]"
+      v-for="(item, index) in stepArray"
       :key="index"
     >
       <div class="width-100 mt-b">
-        <p class="title">{{item}}</p>
+        <p class="title">{{ item }}</p>
       </div>
-      <div v-if="errorStep<=0" class="width-100">
+      <div v-if="errorStep <= 0" class="width-100">
         <div
           class
-          v-bind:class="[index<currentVal ? 'color-pick circle' : 'color-no-pick circle-no-border', '']"
+          v-bind:class="[
+            index < currentVal
+              ? 'color-pick circle'
+              : 'color-no-pick circle-no-border',
+            '',
+          ]"
         ></div>
         <p
-          v-show="index!=0"
+          v-show="index != 0"
           class="line-1"
-          v-bind:class="[index<currentVal ? 'color-pick' : 'color-no-pick', '']"
+          v-bind:class="[
+            index < currentVal ? 'color-pick' : 'color-no-pick',
+            '',
+          ]"
         ></p>
         <p
-          v-show="index!=stepArray.length-1"
+          v-show="index != stepArray.length - 1"
           class="line-2"
-          v-bind:class="[index<currentVal-1 ? 'color-pick' : 'color-no-pick', '']"
+          v-bind:class="[
+            index < currentVal - 1 ? 'color-pick' : 'color-no-pick',
+            '',
+          ]"
         ></p>
       </div>
       <div v-else class="width-100">
         <div
           class
-          v-bind:class="[index<errorStep ? 'color-pick circle' : index==errorStep?'color-pick circle-error' :'color-no-pick circle-no-border', '']"
+          v-bind:class="[
+            index < errorStep
+              ? 'color-pick circle'
+              : index == errorStep
+              ? 'color-pick circle-error'
+              : 'color-no-pick circle-no-border',
+            '',
+          ]"
         ></div>
         <p
-          v-show="index!=0"
+          v-show="index != 0"
           class="line-1"
-          v-bind:class="[index<errorStep ? 'color-pick' :index==errorStep?'color-error-pick-1' :'color-no-pick', '']"
+          v-bind:class="[
+            index < errorStep
+              ? 'color-pick'
+              : index == errorStep
+              ? 'color-error-pick-1'
+              : 'color-no-pick',
+            '',
+          ]"
         ></p>
         <p
-          v-show="index!=stepArray.length-1"
+          v-show="index != stepArray.length - 1"
           class="line-2"
-          v-bind:class="[index<errorStep-1 ? 'color-pick' :index==errorStep-1?'color-error-pick-2' :'color-no-pick', '']"
+          v-bind:class="[
+            index < errorStep - 1
+              ? 'color-pick'
+              : index == errorStep - 1
+              ? 'color-error-pick-2'
+              : 'color-no-pick',
+            '',
+          ]"
         ></p>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    currentVal: {
+      type: Number,
+      default: 6,
+      //结束步骤条
+    },
+    errorStep: {
+      type: Number,
+      default: 4,
+    },
+    stepArray: {
+      type: Array,
+      default: function () {
+        return ["步骤1", "步骤2", "步骤3", "步骤4", "步骤5"];
+      },
+    },
+  },
+};
+</script>
 <style scoped >
 .steps {
   background: #fff;
@@ -70,7 +134,7 @@
   top: 3px;
   left: 50%;
 }
-.mt-b{
+.mt-b {
   margin-bottom: 4px;
 }
 .color-pick {
@@ -178,27 +242,3 @@
   border-radius: 50%;
 }
 </style>
-<script>
-export default {
-  data() {
-    return {};
-  },
-  props: {
-    currentVal: {
-      type: Number,
-      default: 6
-      //结束步骤条
-    },
-    errorStep: {
-      type: Number,
-      default: 4
-    },
-    stepArray: {
-      type: Array,
-      default: function() {
-        return ["步骤1", "步骤2", "步骤3", "步骤4","步骤5"];
-      }
-    }
-  }
-};
-</script>
